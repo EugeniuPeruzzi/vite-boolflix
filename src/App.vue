107edf8a9639a33edb838b75c51cb4b9f7619ef6
EugeniuPeruzzi <jaka1997@live.it>
2023-06-26 12:27:06 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<script>
+import axios from 'axios';
+import { store } from './store.js';
+import AppFilmMain from './components/AppFilmMain.vue';
+
+export default {
+  // dico di utilizzare le componenti chiamate
+  components: {
+    AppFilmMain,
+  },
+  data() {
+    return {
+      store,
+    }
+  },
+  mounted() {
+
+  },
+  methods: {
+
+  }
+}
+</script>
+
+<template>
+  <AppFilmMain />
+</template>
+
+<style lang="scss">
+@use './styles/generals.scss' as *;
+</style>
