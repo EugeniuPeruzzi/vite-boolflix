8528f83ede207c15c6738b2ac333744603fd0f7b
EugeniuPeruzzi <jaka1997@live.it>
2023-06-26 17:17:05 +0200

    milestone 3 fixed

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -20,10 +20,12 @@ export default {
                     <div class="film">
                         <ul>
                             <li class="" v-for="(film, index) in store.filmsObj" :key="index">
+                                <img :src="`https://image.tmdb.org/t/p/w342/${film.poster_path}`" alt="">
                                 <div class="film d-flex">
                                     <div class="me-3">{{ film.original_title }}</div>
                                     <div class="me-3">{{ film.original_title }}</div>
-                                    <img :src="`../../node_modules/country-flag-icons/1x1/${film.original_language.toUpperCase()}.svg`"
+                                    <img class="cntr-flag"
+                                        :src="`../../node_modules/country-flag-icons/1x1/${film.original_language.toUpperCase()}.svg`"
                                         :alt="film.original_language.toUpperCase()">
                                     <div class="me-3">{{ film.vote_average }}</div>
                                 </div>
@@ -36,9 +38,11 @@ export default {
                         <ul>
                             <li v-for="(tv, index) in store.tvsObj" :key="index">
                                 <div class="tv d-flex">
+                                    <img :src="`https://image.tmdb.org/t/p/w342/${tv.poster_path}`" alt="">
                                     <div class="me-3">{{ tv.name }}</div>
                                     <div class="me-3">{{ tv.original_name }}</div>
-                                    <img :src="`../../node_modules/country-flag-icons/1x1/${tv.original_language.toUpperCase()}.svg`"
+                                    <img class="cntr-flag"
+                                        :src="`../../node_modules/country-flag-icons/1x1/${tv.original_language.toUpperCase()}.svg`"
                                         :alt="tv.original_language.toUpperCase()">
                                     <div class="me-3">{{ tv.vote_average }}</div>
                                 </div>
@@ -54,6 +58,10 @@ export default {
 
 <style lang="scss" scoped>
 img {
+    width: 20%;
+}
+
+.cntr-flag {
     width: 20px;
     height: 15px;
 }
