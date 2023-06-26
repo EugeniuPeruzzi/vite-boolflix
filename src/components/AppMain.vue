4cde93972acd426ada034152cafe38a3ee21441f
EugeniuPeruzzi <jaka1997@live.it>
2023-06-26 16:27:54 +0200

    flags added, implementing tv search

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,12 +1,13 @@
 <script>
 import { store } from '../store.js';
-
-
 export default {
     data() {
         return {
             store,
         }
+    },
+    computed: {
+
     }
 }
 </script>
@@ -16,16 +17,34 @@ export default {
         <div class="container">
             <div class="row">
                 <div class="col">
-                    <ul>
-                        <li v-for="(film, index) in store.filmsObj" :key="index">
-                            <div class="film d-flex">
-                                <div class="me-3">{{ film.original_title }}</div>
-                                <div class="me-3">{{ film.original_title }}</div>
-                                <div class="me-3">{{ film.original_language }}</div>
-                                <div class="me-3">{{ film.vote_average }}</div>
-                            </div>
-                        </li>
-                    </ul>
+                    <div class="film">
+                        <ul>
+                            <li class="m-4" v-for="(film, index) in store.filmsObj" :key="index">
+                                <div class="film d-flex">
+                                    <div class="me-3">{{ film.original_title }}</div>
+                                    <div class="me-3">{{ film.original_title }}</div>
+                                    <img :src="`../../node_modules/country-flag-icons/1x1/${film.original_language.toUpperCase()}.svg`"
+                                        :alt="film.original_language.toUpperCase()">
+                                    <div class="me-3">{{ film.vote_average }}</div>
+                                </div>
+
+                            </li>
+                        </ul>
+                    </div>
+
+                    <div class="tv">
+                        <ul>
+                            <li v-for="(tv, index) in store.filmsObj" :key="index">
+                                <div class="tv d-flex">
+                                    <div class="me-3">{{ tv.name }}</div>
+                                    <div class="me-3">{{ tv.original_name }}</div>
+                                    <img :src="`../../node_modules/country-flag-icons/1x1/${tv.original_language.toUpperCase()}.svg`"
+                                        :alt="tv.original_language.toUpperCase()">
+                                    <div class="me-3">{{ tv.vote_average }}</div>
+                                </div>
+                            </li>
+                        </ul>
+                    </div>
                 </div>
             </div>
         </div>
@@ -33,4 +52,17 @@ export default {
     </div>
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+img {
+    width: 20px;
+    height: 15px;
+}
+
+.tv {
+    background-color: purple;
+}
+
+.film {
+    background-color: blue;
+}
+</style>
--- src/store.js
+++ src/store.js
@@ -1,7 +1,7 @@
 import { reactive } from 'vue'
 
 export const store = reactive({
-    apiUrl: 'https://api.themoviedb.org/3/search/movie?api_key=4b67cbd6fbd947d07786332b3eb7ee5a',
+    apiUrl: 'https://api.themoviedb.org/3/search/multi?api_key=4b67cbd6fbd947d07786332b3eb7ee5a',
     // searchUrl: '',
     apiKey: '4b67cbd6fbd947d07786332b3eb7ee5a',
     filmsObj: [],
